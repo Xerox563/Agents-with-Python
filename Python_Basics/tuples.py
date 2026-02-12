12499d93d01267346febea0d4348a90c94557451
amit-i8cloud <agangwar@i8cloudconsulting.com>
2026-02-13 01:06:03 +0530

    [ADD] code for tuples ..

---
+++ Python_Basics/tuples.py
@@ -0,0 +1,52 @@
+arr = (1,2,3,4)
+
+# brr = ('a','b','c')
+print(arr[0])
+print(arr[-1])
+
+# slicing
+print(arr[0:2])
+print(arr[::-1])
+
+
+# total_vals = arr + brr
+# print("After Joining Vals: ",total_vals)
+# # arr[0] = 'z'; # immutable
+# access_val = arr[0];
+# print(access_val)
+
+# # conversion : [tuples -> list] then perform all the operations on list as you want ..
+# y = list(brr)
+# y.append('Hello')
+# t_tuple = tuple(y)
+# print(t_tuple)
+
+# fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+# (green, *tropic, red) = fruits
+
+# print(green)
+# print(tropic)
+# print(red)
+# print(fruits.count("apple"))
+# print(fruits.index("mango"))
+
+
+# looping 
+# for x in arr:
+#     print(x)
+
+# for i in range(len(arr)):
+#     print(i,arr[i])
+
+# i = 0;
+# while(i < len(arr)):
+#     print(i,arr[i])
+#     i+=1
+
+# membership // returns true/false
+print(20 in arr)
+
+
+
+
